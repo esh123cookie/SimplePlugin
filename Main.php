0ea2805d088c792e5b6aa7764e5fcc0b3c30fc73
Alyx903 <60302146+Alyx903@users.noreply.github.com>
2020-04-07 19:16:50 -0400

    Add files via upload

---
+++ Main.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Alyx903\SimplePlugin;
+
+use pocketmine\Server;
+use pocketmine\Player;
+
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+
+use pocketmine\utils/TextFormat;
+
+class Main extends Pluginbase implements Listener {
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool{
+      switch($cmd->getName()){
+        case "test":
+          if($sender instanceof Player) {
+            // real player
+            $sender->sendMessage(TextFormat::GREEN . "Hello " . $sender->getName())
+          } else {
+            
+          }
+      break;
+      }
+      
+      return true;
+    }
+  
+  public function onJoin(PlayerJoinEvent $event){
+    $player = $event->getPlayer();
+    
+    $player->sendMessage(TextFormat::AQUA . "Hello " . $player->getName());
+  }
+
+
+
+}
